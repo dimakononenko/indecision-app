3ec94c5f2bad3502c1293c05dc8815001c571a80
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2017-12-18 03:36:29 -0800

    Second Commit. I hope to see thos comment as first.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.log('App.js is running, but not really! Lets check');
+console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
@@ -14,7 +14,7 @@ var template = React.createElement(
   React.createElement(
     "p",
     null,
-    "This is some info"
+    "\u041D\u0430\u043A\u043E\u043D\u0435\u0446-\u0442\u043E \u043C\u043E\u0439 \u043A\u043E\u0434 \u0440\u0430\u0431\u043E\u0442\u0430\u0435\u0442!"
   ),
   React.createElement(
     "ol",
--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-console.log('App.js is running, but not really! Lets check');
+console.log('App.js is running!');
 
 // JSX - JavaScript XML
 var template = (
   <div>
     <h1>Indecision App</h1>
-    <p>This is some info</p>
+    <p>Наконец-то мой код работает!</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
