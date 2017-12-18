d62197ddb075a6ea2066928a680ed4edb3ea1a9d
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2017-12-18 03:32:36 -0800

    First commit. Working environment.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log('App.js is running, but not really! Lets check');
+
+// JSX - JavaScript XML
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
