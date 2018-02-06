97214e865075ab53a5bd35533eb6139c621b6fea
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2018-02-06 01:13:53 -0800

    Styled Modal window

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-
+import React from "react";
 
 export default class AddOption extends React.Component {
   state = {
@@ -10,8 +8,8 @@ export default class AddOption extends React.Component {
     super(props);
     this.handleAddOption = this.handleAddOption.bind(this);
   }
-  
-  handleAddOption = (e) => {
+
+  handleAddOption = e => {
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option);
@@ -24,16 +22,17 @@ export default class AddOption extends React.Component {
       e.target.elements.option.value = "";
     }
   };
-    render() {
-      return (
-        <div>
-          {this.state.error && <p className="add-option">{this.state.error}</p>}
-          <br />
-          <form onSubmit={this.handleAddOption}>
-            <input type="text" name="option" />
-            <button className='button'>Add option</button>
-          </form>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.error && (
+          <p className="add-option-error">{this.state.error}</p>
+        )}
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
+          <button className="button">Add option</button>
+        </form>
+      </div>
+    );
   }
+}
